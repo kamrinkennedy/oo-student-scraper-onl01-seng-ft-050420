d8a7474ed75ab661a4fc67ad1ea28be87e2620a5
Kamrin Kennedy <kamrinkennedy@gmail.com>
2020-05-23 00:37:34 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,9 @@ class Student
   @@all = []
 
   def initialize(student_hash)
-    
+    self.name = student_hash[:name]
+    self.location = student_hash[:location]
+    self.profile_url = student_hash[:profile_url]
   end
 
   def self.create_from_collection(students_array)
