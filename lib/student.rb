0467f10ca6cadeb0bb2a93c1a8ec9dc6b30056ae
Kamrin Kennedy <kamrinkennedy@gmail.com>
2020-05-23 00:38:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -8,6 +8,7 @@ class Student
     self.name = student_hash[:name]
     self.location = student_hash[:location]
     self.profile_url = student_hash[:profile_url]
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
